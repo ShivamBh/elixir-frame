ec84685413c18f29c24d2c65b07f08f1b8f2312b
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-11-02 14:10:24 +0530

    init and add project deps

---
+++ lib/elxframe.ex
@@ -0,0 +1,18 @@
+defmodule Elxframe do
+  @moduledoc """
+  Documentation for `Elxframe`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elxframe.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elxframe_test.exs
@@ -0,0 +1,8 @@
+defmodule ElxframeTest do
+  use ExUnit.Case
+  doctest Elxframe
+
+  test "greets the world" do
+    assert Elxframe.hello() == :world
+  end
+end
